fc6a8db253a535222a625344c9a99cbb0f9d8a24
SherryH <enjoy.wind7rain@gmail.com>
2025-04-08 20:19:04 +0800

    Completed progressbar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,77 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
-const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+const MIN = 0;
+const MAX = 100;
+const clamp = (value) => {
+  return Math.min(Math.max(value, MIN), MAX);
+};
+
+const SIZES = {
+  small: {
+    height: '8px',
+    padding: 0,
+    'border-radius': '4px',
+  },
+  medium: {
+    height: '12px',
+    padding: 0,
+    'border-radius': '4px',
+  },
+  large: {
+    height: '24px',
+    padding: '4px',
+    'border-radius': '8px',
+  },
+};
+
+const StyledProgress = styled.div`
+  width: 370px;
+  height: var(--height);
+  background: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  border-radius: var(--radius);
+  padding: var(--padding);
+`;
+
+const StyledIndicator = styled.div`
+  width: var(--width);
+  background-color: ${COLORS.primary};
+  height: 100%;
+`;
+
+const BarWrapper = styled.div`
+  overflow: hidden;
+  border-radius: 4px;
+  width: 100%;
+  height: 100%;
+`;
+
+const ProgressBar = ({ value: inputValue, size: inputSize }) => {
+  const size = SIZES[inputSize];
+
+  const value = clamp(inputValue);
+  return (
+    <StyledProgress
+      role="progressbar"
+      aria-label="ProgresBar"
+      value={value}
+      aria-valuemin="0"
+      aria-valuenow="50"
+      aria-valuemax="100"
+      style={{
+        '--width': value + '%',
+        '--height': size.height,
+        '--padding': size.padding,
+        '--radius': size['border-radius'],
+      }}
+    >
+      <BarWrapper>
+        <StyledIndicator></StyledIndicator>
+      </BarWrapper>
+      <VisuallyHidden>{value}</VisuallyHidden>
+    </StyledProgress>
+  );
 };
 
 export default ProgressBar;
