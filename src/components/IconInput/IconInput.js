f678947fa440dc922a2590baa917e43b48195ced
SherryH <enjoy.wind7rain@gmail.com>
2025-04-10 23:47:35 +0800

    Completed hardcoded size m IconInput

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,14 +6,54 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const StyledInput = styled.input`
+  border: none;
+  padding-left: 16px;
+  height: 30px;
+  width: var(--width);
+  color: inherit;
+  outline-offset: 2px;
+`;
+
+const Wrapper = styled.div`
+  position: relative;
+  border-bottom: 1px solid black;
+  width: var(--width);
+  color: ${COLORS.gray700};
+  :hover {
+    color: black;
+    font-weight: 700;
+  }
+`;
+
+const IconWrapper = styled.div`
+  width: 16px;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  height: 16px;
+  color: inherit;
+`;
+
 const IconInput = ({
   label,
   icon,
   width = 250,
   size,
+  value,
   placeholder,
+  ...rest
 }) => {
-  return 'TODO';
+  return (
+    <Wrapper style={{ '--width': width + 'px' }}>
+      <IconWrapper>
+        <Icon id={icon} size="16px" />
+      </IconWrapper>
+      <StyledInput {...rest}>{value}</StyledInput>
+      <VisuallyHidden>{label}</VisuallyHidden>
+    </Wrapper>
+  );
 };
 
 export default IconInput;
