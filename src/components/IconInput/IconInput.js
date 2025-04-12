594c551f43cfbaf84457a8f98db2d534f0702195
SherryH <enjoy.wind7rain@gmail.com>
2025-04-12 12:42:50 +0800

    Added large variant

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,33 +6,52 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const SIZES = {
+  small: {
+    height: '24px',
+    iconWidth: '16px',
+    iconGap: '24px',
+    borderBottom: '1px solid black',
+  },
+  large: {
+    height: '36px',
+    iconWidth: '24px',
+    iconGap: '36px',
+    borderBottom: '2px solid black',
+  },
+};
+
 const StyledInput = styled.input`
   border: none;
-  padding-left: 16px;
-  height: 30px;
+  padding-left: var(--iconGap);
+  height: var(--height);
   width: var(--width);
   color: inherit;
   outline-offset: 2px;
+  &:placeholder {
+    color: ${COLORS.gray500};
+    font-weight: 400;
+  }
 `;
 
 const Wrapper = styled.div`
   position: relative;
-  border-bottom: 1px solid black;
+  border-bottom: var(--borderBottom);
   width: var(--width);
   color: ${COLORS.gray700};
-  :hover {
+  font-weight: 700;
+  &:hover {
     color: black;
-    font-weight: 700;
   }
 `;
 
 const IconWrapper = styled.div`
-  width: 16px;
+  height: var(--iconWidth);
+  width: var(--iconWidth);
   position: absolute;
   top: 0;
   bottom: 0;
   margin: auto;
-  height: 16px;
   color: inherit;
 `;
 
@@ -40,15 +59,23 @@ const IconInput = ({
   label,
   icon,
   width = 250,
-  size,
+  size = 'small',
   value,
-  placeholder,
   ...rest
 }) => {
+  const { height, iconGap, iconWidth, borderBottom } = SIZES[size];
   return (
-    <Wrapper style={{ '--width': width + 'px' }}>
+    <Wrapper
+      style={{
+        '--width': width + 'px',
+        '--height': height,
+        '--iconGap': iconGap,
+        '--iconWidth': iconWidth,
+        '--borderBottom': borderBottom,
+      }}
+    >
       <IconWrapper>
-        <Icon id={icon} size="16px" />
+        <Icon id={icon} size={iconWidth} />
       </IconWrapper>
       <StyledInput {...rest}>{value}</StyledInput>
       <VisuallyHidden>{label}</VisuallyHidden>
